4f1fcef8c452e196a119162649860e7650acbf24
JaredVS777 <jared.valenzuelajvs02@gmail.com>
2024-05-17 16:55:47 -0500

    Prueba

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCzpRW4pl90wTQJBKTAhOAoIsMjujhNnFU",
+  authDomain: "proyecto1-3667a.firebaseapp.com",
+  projectId: "proyecto1-3667a",
+  storageBucket: "proyecto1-3667a.appspot.com",
+  messagingSenderId: "225499054783",
+  appId: "1:225499054783:web:3fcbffb44628f4eb6dbc0a",
+  measurementId: "G-KMW6RLWEWF"
+  },
+};
